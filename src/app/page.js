4e0dd404a6992a0c9101c678b62ea570701a0548
Gabriel Torres <Gabrieltorres2014@gmail.com>
2023-11-21 11:32:29 -0400

    wtf

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default async function Home({searchParams}) {
 
   async function getProducts (id) {
 
-    const {data} = await axios(`${headersList.get('x-forwarded-proto')}://${headersList.get('host')}/api/products?id=${id}`).then( response => response).catch(error => console.log(error));
+    const {data} = await axios(`http://${headersList.get('host')}/api/products?id=${id}`).then( response => response).catch(error => console.log(error));
     return data
   }
   let products = [];
