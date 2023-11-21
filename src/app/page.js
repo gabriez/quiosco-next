93bc9cf9e6fba2e9f16e4dab293d7644d7736afd
Gabriel Torres <Gabrieltorres2014@gmail.com>
2023-11-21 16:49:10 -0400

    c

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default async function Home({searchParams}) {
 
     const data = await fetch(`http://${headersList.get('host')}/api/products?id=${id}`).then(  res =>
     {
-      if (new Headers(res.headers).get('content-type') === 'text/plain;charset=UTF-8'){
+      if (new Headers(res.headers).get('content-type') === 'text/html;charset=UTF-8'){
         return res
       }
       return res.json()
